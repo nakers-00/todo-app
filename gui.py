056a9b0cdfe342b37365d9715b5f5049f9403760
3030n <nakers9@wgu.edu>
2024-07-18 17:51:28 -0400

    Implemented the Edit Button

--- gui.py
+++ gui.py
@@ -5,19 +5,37 @@ label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 exit_button = sg.CloseButton("Close")
+list_box = sg.Listbox(values=functions.read_file(), key="todos",
+                      enable_events=True, size=(45, 10))
+edit_button = sg.Button("Edit")
 
 window = sg.Window('My To-Do App',
-                   layout=[[label], [input_box, add_button], [exit_button]],
+                   layout=[[label], [input_box, add_button], [list_box, edit_button], [exit_button]],
                    font=('Helvetica', 14))
 while True:
     event, values = window.read()
-
+    print(1, event)
+    print(2, values)
+    print(3, values['todos'])
     match event:
         case "Add":
             todos = functions.read_file()
             new_todo = values['todo'] + "\n"
-            todos.append(new_todo)
+            todos.append(new_todo.title())
             functions.write_file(todos)
+            window['todos'].update(values=todos)
+
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+
+            todos = functions.read_file()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo.title() + "\n"
+            functions.write_file(todos)
+            window['todos'].update(values=todos)
+        case "todos":
+            window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
             break
 
