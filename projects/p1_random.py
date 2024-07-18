d22b5f251557a64e492276e53577b14439bafa9d
3030n <nakers9@wgu.edu>
2024-07-18 16:15:40 -0400

    Add projects folder to gitignore

--- projects/p1_random.py
+++
@@ -1,9 +0,0 @@
-import random
-
-low_num = int(input("Enter the lower bound: "))
-high_num = int(input("Enter the upper bound: "))
-
-
-rand_num = random.randrange(low_num, high_num)
-
-print(rand_num)
